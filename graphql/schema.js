3eae119a876d8bb02bb7f3a1076e70b4b7f35b56
John Griffith <john@John-MacBook-Air.local>
2022-07-20 11:13:13 -0400

    Defined a mutation schema.

--- graphql/schema.js
+++ graphql/schema.js
@@ -10,7 +10,41 @@ module.exports = buildSchema(`
         hello: TestData!
     }
 
+    type Post {
+        _id: ID!
+        title: String!
+        content: String!
+        imageUrl: String!
+        creator: User!
+        createdAt: String!
+        updatedAt: String!
+    }
+
+    type User {
+        _id: ID!
+        email: String!
+        name: String!
+        password: String
+        status: String!
+        posts: [Post!]!
+    }
+
+    input UserInputData {
+        email: String!
+        name: String!
+        password: String!
+    }
+
+    type RootMutation {
+        createUser(userInput: UserInputData): User!
+    }
+
     schema {
         query: RootQuery
+        mutation: RootMutation
     }
 `);
+
+/*
+{ "query": "{ hello { text views } }" }
+*/
