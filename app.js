001d244f2961f468a039a1e0216b3bda59feddd0
John Griffith <john@John-MacBook-Air.local>
2022-07-20 12:01:06 -0400

    Replaced default error with a custom error object.

--- app.js
+++ app.js
@@ -52,6 +52,15 @@ app.use('/graphql', graphqlHTTP({
   schema: graphqlSchema,
   rootValue: graphqlResolver,
   graphiql: true, // Allows the graphiql GUI when opening /graphql in the browser
+  formatError(err) {
+    if (!err.originalError) {
+      return err;
+    }
+    const data = err.originalError.data;
+    const message = err.message || 'An error occurred';
+    const code = err.originalError.code || 500;
+    return { message: message, status: code, data: data };
+  }
 }));
 
 app.use((error, req, res, next) => {
