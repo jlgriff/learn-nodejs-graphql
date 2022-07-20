46407bb0dc3a24085db59364849bd44e0533ce37
John Griffith <john@John-MacBook-Air.local>
2022-07-20 12:48:22 -0400

    Added a resolver for creating a post.

--- graphql/schema.js
+++ graphql/schema.js
@@ -31,12 +31,19 @@ module.exports = buildSchema(`
         password: String!
     }
 
+    input PostInputData {
+        title: String!
+        content: String!
+        imageUrl: String
+    }
+
     type RootQuery {
         login(email: String!, password: String!): AuthData!
     }
 
     type RootMutation {
         createUser(userInput: UserInputData): User!
+        createPost(postInput: PostInputData): Post!
     }
 
     schema {
@@ -60,4 +67,12 @@ mutation {
     userId
   }
 }
+
+mutation {
+  createPost(postInput: {title: "Title", content: "Content"}) {
+    _id
+    title
+  }
+}
+
 */
